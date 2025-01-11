3d519b64f9870cb2146f4d6574ac72e43c9de1cf
Elijah Wilson <elijahwilsonnn@gmail.com>
2025-01-10 16:16:30 -0800

    [STYLING]: new styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,18 +19,50 @@
 
 <section class="hero-bg flex h-[40rem] flex-col items-center bg-black/60">
 	<div class="pt-48">
-		<h1 class="font-hero z-auto mx-auto text-center text-[120px] leading-[90%] text-white">
+		<!-- <h1 class="font-hero z-auto mx-auto text-center text-[120px] leading-[90%] text-white">
 			Yaqui
-		</h1>
-		<h2 class=" mx-auto text-center text-4xl font-medium text-white">Coffee House</h2>
-		<div class="flex flex-row items-center justify-center gap-2 pt-5">
-			<Button class="	bg-amber-600">Order Online</Button>
-			<Button variant="secondary">Get Directions</Button>
-		</div>
+		</h1> -->
+		<!-- <h2 class=" mx-auto text-center text-4xl font-medium text-white uppercase">Coffee House</h2> -->
+		<!-- <div class="flex flex-row items-center justify-center gap-2 pt-5">
+			<Button size="lg" class="	bg-amber-600">Order Online</Button>
+			<Button size="lg" variant="secondary">Get Directions</Button>
+		</div> -->
 	</div>
 </section>
 
-<section class="mx-auto my-10 max-w-[100rem]">
+<section class="mx-auto my-10">
+	<div class="my-20 flex flex-col items-center justify-center">
+		<h3
+			class=" mb-5 text-center text-4xl font-semibold capitalize leading-[125%] tracking-[-0.96px]"
+		>
+			Visit Us
+		</h3>
+		<a
+			href="https://maps.app.goo.gl/Cz7kFZg73Kx4T1tk6"
+			target="_blank"
+			rel="noopener noreferrer"
+			class="text-md mb-4 cursor-pointer text-center text-lg font-light leading-[150%] tracking-[110%] underline transition duration-150 ease-in-out hover:text-gray-500"
+		>
+			1930 Avenida Escaya suite 101, Chula Vista, CA 91913
+		</a>
+		<div class="flex flex-col items-center justify-center gap-1.5 leading-[150%] tracking-[110%]">
+			<div class="flex flex-row items-center justify-center gap-2 text-lg font-light">
+				<p>M-F</p>
+				<p>|</p>
+				<p>6am-4pm</p>
+			</div>
+			<div class="flex flex-row items-center justify-center gap-2 text-lg font-light">
+				<p>S-S</p>
+				<p>|</p>
+				<p>7am-4pm</p>
+			</div>
+			<div class="flex flex-row items-center justify-center gap-2 text-lg font-light">
+				<p>Kitchen</p>
+				<p>|</p>
+				<p>7am-1pm (Everyday)</p>
+			</div>
+		</div>
+	</div>
 	<div
 		class="sm: mx-auto flex h-[20rem] w-full flex-row items-center gap-2 px-2 sm:h-[30rem] sm:px-8 md:h-[35rem]"
 	>
@@ -40,25 +72,39 @@
 			<img src={section1image3} alt="" class="h-1/2 object-cover" />
 		</div>
 	</div>
+	<div class=" my-20 flex flex-col justify-center gap-5 px-8 lg:flex-row">
+		<h3 class=" text-center text-4xl font-semibold capitalize leading-[125%] tracking-[-0.96px]">
+			Start Your Day Right.
+		</h3>
+		<p class="max-w-5xl text-lg font-extralight leading-[150%] tracking-[110%]">
+			Nestled in the heart of Chula Vista, CA, Yaqui Coffee House exudes warmth and welcomes guests
+			with open arms. With its cozy atmosphere, friendly staff, and a menu bursting with
+			mouthwatering dishes, it's the perfect place to unwind and savor a delightful meal. Our
+			diverse menu caters to every palate, whether you're looking for a leisurely dinner with loved
+			ones, a quick lunch catch-up with friends, or a solo meal at the bar. Yaqui Coffee House
+			offers a relaxed setting where laughter flows as freely as the conversation. Come as you are,
+			and leave with a satisfied heart, a full stomach, and a smile.
+		</p>
+	</div>
 
-	<div class="mx-4 mt-12 flex-row sm:mx-8 md:flex md:space-x-10">
-		<img src={aLittleBitAboutUs} alt="" class="hidden w-2/3 rounded-sm object-cover lg:block" />
+	<div class="mt-12 flex-row px-8 md:flex md:space-x-10">
+		<img src={aLittleBitAboutUs} alt="" class=" rounded-sm object-cover md:w-2/3 lg:block" />
 
-		<div class="mt-4 flex flex-col items-center space-y-10">
+		<div class="mt-8 flex flex-col items-center space-y-10">
 			<div class="flex flex-col md:space-y-5">
 				<div class="flex flex-row items-center">
-					<span class="mr-3 h-28 w-1 rounded-xl bg-[#1e2a4a] sm:h-20 lg:h-28"></span>
-					<h3 class=" text-4xl font-medium capitalize leading-[125%] tracking-[-0.96px]">
+					<!-- <span class="mr-3 h-28 w-1 rounded-xl bg-[#1e2a4a] sm:h-20 lg:h-28"></span> -->
+					<h3 class="text-4xl font-semibold capitalize leading-[125%] tracking-[-0.96px]">
 						So Comfortable, You Might Forget You're Not at Home.
 					</h3>
 				</div>
-				<p class="text-normal font-normal leading-[150%]">
+				<p class="max-w-5xl text-lg font-extralight leading-[150%] tracking-[110%]">
 					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam voluptates eos autem
 					sunt, magnam quae atque similique dolore! Provident aliquam maiores cumque ipsa minus
 					adipisci obcaecati mollitia tempora et deserunt?
 				</p>
 			</div>
-			<div class="flex flex-col md:space-y-5">
+			<!-- <div class="flex flex-col md:space-y-5">
 				<div class="flex flex-row items-center">
 					<span class="mr-3 h-10 w-1 rounded-xl bg-[#1e2a4a]"></span>
 					<h3 class=" text-4xl font-medium capitalize leading-[125%] tracking-[-0.96px]">
@@ -71,13 +117,11 @@
 					adipisci obcaecati mollitia tempora et deserunt?
 				</p>
 				<p
-					class="font-light underline decoration-neutral-800 transition duration-100 ease-in hover:text-neutral-500 hover:decoration-neutral-500"
+					class="cursor-pointer font-light underline decoration-neutral-800 transition duration-100 ease-in hover:text-neutral-500 hover:decoration-neutral-500"
 				>
-					Explore our story ->
+					Explore our story →
 				</p>
-			</div>
+			</div> -->
 		</div>
 	</div>
-
-	<div class=""></div>
 </section>
